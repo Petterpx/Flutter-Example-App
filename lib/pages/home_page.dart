f83e232be229a93896423f6dfe7b462ded8fe4dc
petterp <shiyihuicloud@163.com>
2020-10-08 23:38:51 +0800

    feat:自定义AppBar实现滚动监听及透明度渐变

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,8 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+const APPBAR_SCROLL_OFFSET = 100;
+
 class _HomePageState extends State<HomePage> {
   List _imageUrls = [
     'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2171756432,1932813666&fm=26&gp=0.jpg',
@@ -16,28 +18,84 @@ class _HomePageState extends State<HomePage> {
         'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1765232562,2313752077&fm=26&gp=0.jpg'
   ];
 
+  double appBarAlpha = 0;
+
+  _onScroll(offset) {
+    //计算当前滚动的比例
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    //到达顶端，继续向上滚动
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(alpha);
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 160,
-                child: Swiper(
-                  itemCount: _imageUrls.length,
-                  autoplay: true,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Image.network(
-                      _imageUrls[index],
-                      //fill 按图片的宽高比填充到view中
-                      fit: BoxFit.fill,
-                    );
-                  },
-                  pagination: SwiperPagination(),
-                ),
-              )
-            ],
+          //默认子元素按照左上角位置排列，类似Android中的 FrameLayout
+          body: Stack(
+        children: [
+          // MediaQuery.removePadding 可以移除padding的
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            //布局监听
+            child: NotificationListener(
+                //监听listView的滚动,如何元素更新，并且是第0个元素时才触发滚动监听，即只有列表滚动才监听
+                //scrollNotification.depth 元素深度
+                // ignore: missing_return
+                onNotification: (scrollNotification) {
+                  if (scrollNotification is ScrollUpdateNotification &&
+                      scrollNotification.depth == 0) {
+                    //滚动且列表在滚动时
+                    _onScroll(scrollNotification.metrics.pixels);
+                  }
+                },
+                //列表布局
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      height: 160,
+                      child: Swiper(
+                        itemCount: _imageUrls.length,
+                        autoplay: true,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Image.network(
+                            _imageUrls[index],
+                            //fill 按图片的宽高比填充到view中
+                            fit: BoxFit.fill,
+                          );
+                        },
+                        pagination: SwiperPagination(),
+                      ),
+                    ),
+                    Container(
+                      height: 800,
+                      child: ListTile(title: Text('test')),
+                    )
+                  ],
+                )),
           ),
-        ),
-      );
+          //透明度设置
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              //背景
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: Text('首页'),
+                ),
+              ),
+            ),
+          )
+        ],
+      ));
 }
