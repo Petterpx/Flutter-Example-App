9359d635d27761c05dda4ad4b18cb5450c19f1f9
petterp <shiyihuicloud@163.com>
2020-10-08 22:52:09 +0800

    feat:加入banner

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # flutter_example_app
 
-This is a sample application written by Flutter
+This is a sample application written by Flutter.
+
+## 使用到的第三方组件
+-Banner-[flutter_swiper](https://github.com/best-flutter/flutter_swiper)
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,13 +8,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //页面控制器
-  final PageController _controller = PageController(initialPage: 0);
+  List _imageUrls = [
+    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2171756432,1932813666&fm=26&gp=0.jpg',
+    ''
+        'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=293024887,2996913267&fm=26&gp=0.jpg',
+    ''
+        'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1765232562,2313752077&fm=26&gp=0.jpg'
+  ];
 
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Center(
-          child: Text("首页"),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 160,
+                child: Swiper(
+                  itemCount: _imageUrls.length,
+                  autoplay: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.network(
+                      _imageUrls[index],
+                      //fill 按图片的宽高比填充到view中
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  pagination: SwiperPagination(),
+                ),
+              )
+            ],
+          ),
         ),
       );
 }
